f4b54eeac1c222e6b3419c1b264cd1e904218a3f
Christian Madsen <christian.em@live.dk>
2021-10-15 14:57:07 +0200

    fixed empty main

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 import 'testpage.dart';
 
-void main() {}
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
