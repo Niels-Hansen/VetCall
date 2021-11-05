a8a6bbc2e9f60a9ccb669d6d6dcf96b227254182
Jakup Karagoz <jakar19@student.sdu.dk>
2021-11-05 16:35:44 +0100

    Added login functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,39 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:vetcall/services/auth_service.dart';
+import 'package:vetcall/test_page.dart';
+import 'package:vetcall/wrapper.dart';
 import 'dart:async';
-import 'test_page.dart';
-import 'login_page.dart';
-import 'account_type_page.dart';
+import 'loginpage.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'VetCall',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primaryColor: Colors.green,
-        ),
-        home: AccountTypePage());
+    return MultiProvider(
+        providers: [
+          Provider<AuthService>(
+            create: (_) => AuthService(),
+          ),
+        ],
+        child: MaterialApp(
+          title: 'Vetcall',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            primaryColor: Colors.green,
+          ),
+          initialRoute: '/',
+          routes: {
+            '/': (context) => Wrapper(),
+            '/login': (context) => LoginPage()
+          },
+          //home: LoginPage()),
+        ));
   }
 }
---
+++ lib/wrapper.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:vetcall/loginpage.dart';
+
+import 'package:vetcall/models/user_model.dart';
+import 'package:vetcall/services/auth_service.dart';
+import 'package:vetcall/test_page.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final authService = Provider.of<AuthService>(context);
+    return StreamBuilder<User?>(
+      stream: authService.user,
+      builder: (_, AsyncSnapshot<User?> snapshot) {
+        if (snapshot.connectionState == ConnectionState.active) {
+          final User? user = snapshot.data;
+          return user == null ? LoginPage() : TestPage(); //Testpage home screen
+        } else {
+          return Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+      },
+    );
+  }
+}
